43a6d5db8e86655face70f35d6cb567085714fdf
Viktor Shchehelskyi <victorshegelsky@gmail.com>
2026-02-04 01:35:31 +0200

    redid

--- src/Pages/TabsPage.tsx
+++ src/Pages/TabsPage.tsx
@@ -1,38 +1,52 @@
-import { Link, Outlet, useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
-const tabs = [
+type Tab = {
+  id: string;
+  title: string;
+  content: string;
+};
+
+const tabs: Tab[] = [
   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
 export const TabsPage = () => {
-  const { tabsId } = useParams();
+  const { tabId } = useParams();
+  const tabFind =
+    tabs && Array.isArray(tabs) ? tabs.find(t => t.id === tabId) : undefined;
 
   return (
     <>
-      <h1 className="title">Tabs page</h1>
-
-      <div className="tabs is-boxed">
-        <ul>
-          {tabs.map(tab => (
-            <li
-              key={tab.id}
-              data-cy="Tab"
-              className={tabsId === tab.id ? 'is-active' : ''}
-            >
-              <Link to={`/tabs/${tab.id}`}>{tab.title}</Link>
-            </li>
-          ))}
-        </ul>
+      <div>
+        <h1 className="title">Tabs page</h1>
+        <div data-cy="TabsComponent"></div>
+        <div className="tabs is-boxed">
+          <ul>
+            {tabs.map(tab => (
+              <li
+                key={tab.id}
+                data-cy="Tab"
+                className={tabId === tab.id ? 'is-active' : ''}
+              >
+                <Link to={`/tabs/${tab.id}`} data-cy="TabLink">
+                  {tab.title}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
 
-      {!tabsId ? (
+      {!tabId ? (
         <div className="block" data-cy="TabContent">
           Please select a tab
         </div>
       ) : (
-        <Outlet context={{ tabs }} />
+        <div className="block" data-cy="TabContent">
+          {tabFind ? tabFind.content : 'Please select a tab'}
+        </div>
       )}
     </>
   );
--- src/Root.tsx
+++ src/Root.tsx
@@ -2,7 +2,6 @@ import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { App } from './App';
 import { HomePage } from './Pages/HomePage';
 import { TabsPage } from './Pages/TabsPage';
-import { TabsDetails } from './components/TabsDetails';
 
 export const Root = () => {
   return (
@@ -11,8 +10,9 @@ export const Root = () => {
         <Route path="/" element={<App />}>
           <Route index element={<HomePage />} />
           <Route path="home" element={<Navigate to="/" replace />} />
-          <Route path="tabs" element={<TabsPage />}>
-            <Route path=":tabsId?" element={<TabsDetails />} />
+          <Route path="tabs">
+            <Route index element={<TabsPage />} />
+            <Route path=":tabId" element={<TabsPage />} />
           </Route>
           <Route path="*" element={<h1 className="title">Page not found</h1>} />
         </Route>
